5e0bd670df06755132f535bb1137f35baf21dece
New-Melo <new_business@outlook.com.br>
2022-01-03 18:43:44 -0300

    first commit

---
+++ src/br/com/bio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.bio;
+
+import br.com.bio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+        /*int a = 2022;
+        System.out.println("Hello World!!! " + a);*/
+    }
+}
+
